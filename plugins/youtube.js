ba2057f5e8a27f743da12cc186cad8f3d1bc67cd
prelink835@gmail.com <prelink835@gmail.com>
2016-12-03 17:20:06 +0100

    puttyfulll

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,16 +1,17 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  GLaDOS youtube module.
+//
+//////////////////////////////////////////////////////////////////////////
 var glados = require('../glados.js');
 module.exports = bot => {
 
     bot.command(glados.main.prefix + 'yt ["multi word argument"]')
         .action((meta, text) => {
-        
-
-//
-
-    glados.main.youTube.search(text, 1, function(error, result) {
-            if (error) {
-                meta.channel.sendMessage(error);
-            } else {
+        glados.main.youTube.search(text, 1, function(error, result) {
+        if (error) {
+            meta.channel.sendMessage(error);
+        } else {
                 try {
                     meta.channel.sendMessage("http://www.youtube.com/watch?v=" + result.items[0].id.videoId);
                 } catch (err) {
@@ -18,8 +19,5 @@ module.exports = bot => {
                 }
             }
         });
-
-    //
-
         });
 };
