9075a1a0b0cdf2f4820c69be3400ea4c0c3b925c
BlackOfWorld <BlackOfWorld@users.noreply.github.com>
2017-03-18 19:40:01 +0100

    Hi :)

--- plugins/force.js
+++ plugins/force.js
@@ -14,11 +14,39 @@ module.exports = bot => {
     cmd.command('shutdown')
         .action((meta) => {
           meta.delete();
-            glados.isAdmin(meta.author.id,meta.guild.id, function(t) {
+            glados.isAdmin(meta.author.id,"GLOBAL", function(t) {
 				if(t == 5)
                     process.exit(1);
 				else
 				return meta.reply('You are not allowed to use this command!');
                 });
         });
+	cmd.command('setusr')
+		.action((meta,text) => {
+		meta.delete();
+		if(meta.channel.guild != undefined)
+			return;
+		glados.isAdmin(meta.author.id,"GLOBAL", function(t) {
+			if(t == 5) {
+				if(text == null)
+					return meta.reply('You cant set username to null...');
+				glados.webusr = text;
+				meta.reply('Sucessfully set administrator username to: ' + text);
+			}
+		});
+	});
+	cmd.command('setpwd')
+		.action((meta,text) => {
+		meta.delete();
+		if(meta.channel.type != 'dm')
+			return;
+		glados.isAdmin(meta.author.id,"GLOBAL", function(t) {
+			if(t == 5) {
+				if(text == null)
+					return meta.reply('You cant set password to null...');
+				glados.webpw = text;
+				meta.reply('Sucessfully set administrator password to: ' + text);
+			}
+		});
+	});
 };
